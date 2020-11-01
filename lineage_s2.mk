91aa49420966a451a4d9375399c66d40ea5fc6d2
Anghirrim <41322380+Anghirrim@users.noreply.github.com>
2020-11-01 09:23:23 +0100

    Update and rename rr_s2.mk to lineage_s2.mk

--- lineage_s2.mk
+++ lineage_s2.mk
@@ -24,13 +24,13 @@ $(call inherit-product, device/leeco/s2/device.mk)
 # Inherit some common ResurrectionRemix stuff.
 TARGET_BOOT_ANIMATION_RES := 1080
 Set TARGET_FACE_UNLOCK_SUPPORTED := true
-$(call inherit-product, vendor/rr/config/common_full_phone.mk)
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
 # Device identifier. This must come after all inclusions.
 PRODUCT_BRAND := LeEco
 PRODUCT_DEVICE := s2
 PRODUCT_MANUFACTURER := LeMobile
-PRODUCT_NAME := rr_s2
+PRODUCT_NAME := lineage_s2
 
 PRODUCT_GMS_CLIENTID_BASE := android-leeco
 
